1219b08eac7eb2f7bd1aae267f00bf060951bf05
Sunday P. Afolabi <31808402+Lecon-Git@users.noreply.github.com>
2021-08-28 04:03:04 -0500

    Logo and others
    Added header to the dashboard of SPSH.

--- play.js
+++ play.js
@@ -2,15 +2,20 @@
 
 var button = document.querySelectorAll(".btn");
 var bar = document.querySelectorAll(".bar");
+var body = document.getElementById("body");
 
 button[0].addEventListener("click", playAudio);
 button[1].addEventListener("click", pauseAudio);
-var audio = new Audio("sounds/audio1.mp3");//check out how to implement audio of any format in javascript
+button[2].addEventListener("click", darkM);
+//var audio = new Audio("sounds/audio1.mp3");//check out how to implement audio of any format in javascript
+var audio = new Audio("sounds/audio2.mkv");//check out how to implement audio of any format in javascript
 
 function playAudio() {
-    window.focus();
+    //window.focus();
     try {
         audio.play();
+        button[0].style.backgroundColor = "antiquewhite";
+        button[1].style.backgroundColor = "peru";
     } catch (error) {
         console.log("This file is not supported: " + error);
     }
@@ -18,4 +23,10 @@ function playAudio() {
 
 function pauseAudio() {
     audio.pause();
+    button[0].style.backgroundColor = "peru";
+    button[1].style.backgroundColor = "antiquewhite";
+}
+
+function darkM() {
+    body.style.backgroundColor = "white";
 }
