04dd057ff81379ae19c4f13958bc1595cbacabca
Sunday P. Afolabi <31808402+Lecon-Git@users.noreply.github.com>
2021-09-24 11:12:02 +0100

    new addition

--- play.js
+++ play.js
@@ -3,12 +3,22 @@
 var button = document.querySelectorAll(".btn");
 var bar = document.querySelectorAll(".bar");
 var body = document.getElementById("body");
+var navText = document.querySelectorAll(".navbar_item a");
+
+
+//var audio = new Audio("sounds/audio1.mp3");//check out how to implement audio of any format in javascript
+var audio = new Audio("sounds/audio1.mp3");//check out how to implement audio of any format in javascript
+
 
 button[0].addEventListener("click", playAudio);
 button[1].addEventListener("click", pauseAudio);
-button[2].addEventListener("click", darkM);
-//var audio = new Audio("sounds/audio1.mp3");//check out how to implement audio of any format in javascript
-var audio = new Audio("sounds/audio1.mp3");//check out how to implement audio of any format in javascript
+button[2].addEventListener("click", darkMW);
+button[3].addEventListener("click", darkMB);
+button[4].addEventListener("click", () => {
+    if (audioLoop) {
+        audio.play();
+    }
+});
 
 function playAudio() {
     //window.focus();
@@ -27,6 +37,28 @@ function pauseAudio() {
     button[1].style.backgroundColor = "antiquewhite";
 }
 
-function darkM() {
+function darkMW() {
     body.style.backgroundColor = "white";
+    body.style.color = "#444";
+    navText.forEach(element => {
+        element.style.color = "#444";
+    });
+}
+
+function darkMB() {
+    body.style.backgroundColor = "black";
+    body.style.color = "peru";
+    navText.forEach(element => {
+        element.style.color = "peru";
+    });
+}
+
+function audioLoop() {
+    // console.log(typeof audio.loop + " and " + audio.loop);
+    if (typeof audio.loop == "boolean") {
+        return true;
+        //console.log(audio.loop);
+    } else {
+        return false;
+    }
 }
